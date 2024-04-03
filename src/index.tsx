ea8426c34d0926827a6ace392d530a585fee1555
om menaria <opmen@Omprakash-hoicko.hoicko.com>
2024-04-03 21:12:44 +0530

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-  <BrowserRouter>
-  {/* <React.StrictMode> */}
-    <App />
-  {/* </React.StrictMode> */}
-  </BrowserRouter>
-);
+import { BrowserRouter } from 'react-router-dom';
 
+ReactDOM.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
